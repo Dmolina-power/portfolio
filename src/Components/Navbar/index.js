48f842fac466f234e71795ef044b26ced37c2cf7
daniel molina <bigdpower@icloud.com>
2023-01-20 09:47:18 -0600

    changed navbar

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -58,7 +58,7 @@ const NavBar = () => {
                 to="/about"
                 onClick={() => updateExpanded(false)}
               >
-                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+                <AiOutlineUser style={{ marginBottom: "2px" }} /> Skills
               </Nav.Link>
             </Nav.Item>
 
